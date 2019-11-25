b59ecbbc87eb81943a826d1946a7da6c40ada64f
grilleddcheesus <edgs93@gmail.com>
2019-11-25 16:44:02 -0500

    First comment for commit

---
+++ src/test/java/com/edwin/cloud/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.cloud.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
